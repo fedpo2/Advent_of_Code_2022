cd71a50b1a1d1e81a051e34bf2efdadfc0c7598d
federico nicolas polidoro <federico.nicolas.polidoro@gmail.com>
2023-08-07 22:53:34 -0300

    Solved Day_1 Part_1

---
+++ day_1/src/main.rs
@@ -0,0 +1,33 @@
+use std::fs::read_to_string;
+
+fn main() {
+    let mut elf_values = decode_elf_calories(vectorizar_txt("input.txt"));
+    elf_values.sort();
+    for value in elf_values {
+    println!("{}", value);
+    }
+}
+
+fn vectorizar_txt(filename: &str) -> Vec<String> {
+    read_to_string(filename)
+        .unwrap()
+        .lines()
+        .map(String::from)
+        .collect()
+}
+
+fn decode_elf_calories(lines: Vec<String>) -> Vec<u32> {
+    let mut valores: Vec<u32> = Vec::new();
+    let mut accumulator: u32 = 0;
+
+    for line in lines {
+        if line.is_empty() {
+            valores.push(accumulator);
+            accumulator = 0;
+            continue;
+        }
+        accumulator += line.parse::<u32>().unwrap();
+    }
+
+    valores
+}
