50003c8b09b16c68b7d0c50f71910a9e0ff5a869
federico nicolas polidoro <federico.nicolas.polidoro@gmail.com>
2023-08-08 00:56:51 -0300

    Add day_2 part_2
    harcodeados los valores porque es espantoso de hacer con enums

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -2,9 +2,13 @@ use std::fs::read_to_string;
 
 
 fn main() {
-    let resultado_parte1 = procesar_turnos(vectorizar_txt("input.txt"));
+    let turnos = vectorizar_txt("input.txt");
+
+    let resultado_parte1 = procesar_turnos(turnos.clone(), true);
     println!("Resultado Parte 1: {}", resultado_parte1);
 
+    let resultado_parte2 = procesar_turnos(turnos, false);
+    println!("Resultado Parte 2: {}", resultado_parte2);
 
 }
 
@@ -17,14 +21,47 @@ fn vectorizar_txt(filename: &str) -> Vec<String> {
         .collect()
 }
 
-fn procesar_turnos(turns: Vec<String>) -> u32 {
+fn procesar_turnos(turns: Vec<String>, parte: bool) -> u32 {
     let mut mis_puntos: u32 = 0;
 
-    for turn in turns {
-        mis_puntos += resultado_turno(turn);
+    if parte {
+        for turn in turns {
+            mis_puntos += resultado_turno(turn);
+        }
+    } else {
+        for turn in turns {
+            mis_puntos += resultado_turno_part2(turn);
+        }
     }
-
     mis_puntos
+}
+
+fn resultado_turno_part2(turn: String) -> u32 {
+    let ret: u32;
+    match turn.chars().next().unwrap() {
+        'A' => match turn.chars().last().unwrap() {
+            'X' => ret = 3,
+            'Y' => ret = 4,
+            'Z' => ret = 8,
+            _ => ret = 0,
+        }
+
+        'B' => match turn.chars().last().unwrap() {
+            'X' => ret = 1,
+            'Y' => ret = 5,
+            'Z' => ret = 9,
+            _ => ret = 0,
+        }
+
+        'C' => match turn.chars().last().unwrap() {
+            'X' => ret = 2,
+            'Y' => ret = 6,
+            'Z' => ret = 7,
+            _ => ret = 0,
+        }
+        _ => ret = 0,
+    }
+    ret
 }
 
 fn resultado_turno(turn: String) -> u32 {
